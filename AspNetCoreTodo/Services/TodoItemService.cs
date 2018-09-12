6a1d783774cd020927f4fcc85e79f2e600842a01
German <german.gilabert@gmail.com>
2018-09-11 22:07:40 -0300

    Clase 11-9-18

--- AspNetCoreTodo/Services/TodoItemService.cs
+++ AspNetCoreTodo/Services/TodoItemService.cs
@@ -1,19 +1,30 @@
 
 
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
-    using AspNetCoreTodo.Data;
-    using AspNetCoreTodo.Models;
-    using Microsoft.EntityFrameworkCore;
-    namespace AspNetCoreTodo.Services
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Data;
+using AspNetCoreTodo.Models;
+using Microsoft.EntityFrameworkCore;
+namespace AspNetCoreTodo.Services
+{
+    public class TodoItemService : ITodoItemService
     {
-        public class TodoItemService : ITodoItemService
+        private readonly ApplicationDbContext _context;
+        public TodoItemService(ApplicationDbContext context) { _context = context; }
+        public async Task<TodoItem[]> GetIncompleteItemsAsync() { return await _context.Items.Where(x => x.IsDone == false).ToArrayAsync(); }
+
+        public async Task<bool> AddItemAsync(TodoItem newItem)
         {
-            private readonly ApplicationDbContext _context;
-            public TodoItemService(ApplicationDbContext context) { _context = context; }
-            public async Task<TodoItem[]> GetIncompleteItemsAsync() { return await _context.Items.Where(x => x.IsDone == false).ToArrayAsync(); }
+            newItem.Id = Guid.NewGuid(); 
+            newItem.IsDone = false; 
+            newItem.DueAt = DateTimeOffset.Now.AddDays(3);
+            _context.Items.Add(newItem);
+            var saveResult = await _context.SaveChangesAsync(); 
+            return saveResult == 1;
+            
         }
     }
+}
 
