bafdcae4a51dac6da2908daac4054584ac3ded4d
German Gilabert <german.gilabert@gmail.com>
2018-10-15 23:57:16 -0300

    Added 4 tests from book, refactor needed

--- AspNetCoreTodo.UnitTests/TodoItemServiceShould.cs
+++ AspNetCoreTodo.UnitTests/TodoItemServiceShould.cs
@@ -13,7 +13,8 @@ namespace AspNetCoreTodo.UnitTests
         [Fact]
         public async Task AddNewItemAsIncompleteWithDueDate()
         {
-            var options = new DbContextOptionsBuilder<AspNetCoreTodo.Data.ApplicationDbContext>().UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
+            var options = new DbContextOptionsBuilder<AspNetCoreTodo.Data.ApplicationDbContext>()
+                        .UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
 
             // Set up a context (connection to the "DB") for writing
             using (var context = new ApplicationDbContext(options))
@@ -41,6 +42,75 @@ namespace AspNetCoreTodo.UnitTests
                 var difference = DateTimeOffset.Now.AddDays(3) - item.DueAt;
                 Assert.True(difference < TimeSpan.FromSeconds(1));
             }
+        }
+
+        [Fact]
+        public async Task MarkDoneWrongId()
+        {
+            var options = new DbContextOptionsBuilder<AspNetCoreTodo.Data.ApplicationDbContext>()
+                        .UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
+            using (var context = new ApplicationDbContext(options))
+            {
+                
+                var service = new TodoItemService(context);
+                var fakeUser = new ApplicationUser
+                {
+                    Id = "fake-000",
+                    UserName = "fake@example.com"
+                };
+                var SecondFakeUser = new ApplicationUser
+                {
+                    Id = "fake-001",
+                    UserName = "fake2@example.com"
+                };
+
+                await service.AddItemAsync(new TodoItem {Title = "Testing?"}, fakeUser);
+                var item = await context.Items.FirstAsync();
+                Assert.False(await service.MarkDoneAsync(item.Id,SecondFakeUser));
+            }
+        }
+
+        [Fact]
+        public async Task MarkDoneCorrectItem()
+        {
+            var options = new DbContextOptionsBuilder<AspNetCoreTodo.Data.ApplicationDbContext>()
+                        .UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {      
+                var service = new TodoItemService(context);
+                var fakeUser = new ApplicationUser
+                {
+                    Id = "fake-000",
+                    UserName = "fake@example.com"
+                };
+
+                await service.AddItemAsync(new TodoItem {Title = "Testing?"}, fakeUser);
+                var item = await context.Items.FirstAsync();
+                Assert.True(await service.MarkDoneAsync(item.Id,fakeUser));
+            }
+        }
+
+          [Fact]
+        public async Task GetItemIncompleteAsycOfTheCorrectUser()
+        {
+            var options = new DbContextOptionsBuilder<AspNetCoreTodo.Data.ApplicationDbContext>().UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {      
+                var service = new TodoItemService(context);
+                var fakeUser = new ApplicationUser
+                {
+                    Id = "fake-000",
+                    UserName = "fake@example.com"
+                };
+
+                await service.AddItemAsync(new TodoItem {Title = "Testing?"}, fakeUser);
+                var item = await context.Items.FirstAsync();
+                TodoItem[] todoItem = await service.GetIncompleteItemsAsync(fakeUser);
+                Assert.True(todoItem[0].UserId == "fake-000");
+            }
+
         }
     }
 }
